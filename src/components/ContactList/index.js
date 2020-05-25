3cd10742bba33c4aa231d2ad67e04a18cfc1e322
an4morph <avgustin456@gmail.com>
2020-05-25 17:25:56 +0600

    Create contact list

--- src/components/ContactList/index.js
+++ src/components/ContactList/index.js
@@ -1,36 +1,54 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
-//import PageWrapper from '../PageWrapper'
-
-
-const contacts = [
-  {id: 1, name: 'Samat', phone: 1223 },
-  {id: 2, name: 'Adyl', phone: 574389 },
-  {id: 3, name: 'Lily', phone: 574389 },
-  {id: 4, name: 'Aidana', phone: 236784 },
-]
+import shortid from 'shortid'
+import Card from './Card'
+import style from './page.module.css'
 
 function ContactList() {
+  const [contacts, setContacts] = React.useState([])
+  const [name, setName] = React.useState('')
+  const [phone, setPhone] = React.useState('')
+
+  const handleAdd = (e) => {
+    e.preventDefault()
+    if (!name || !phone) {
+      alert('заполните все поля')
+      return
+    }
+    const newContact = {
+      id: shortid.generate(),
+      name, 
+      phone
+    }
+    setName('')
+    setPhone('')
+    setContacts([ ...contacts, newContact ])
+  }
+
   return (
-    <div>
-      {
-        contacts.map(c => {
-          return (
-            <NavLink 
-              style={{display: 'block'}} 
-              to={{
-                pathname: `/portfolio/contact-list/${c.id}`,
-                state: { conatct: c }
-              }}
-              key={c.id}
-            >
-              {c.name}
-              &nbsp;
-              {c.phone}
-            </NavLink>
-          )
-        })
-      }
+    <div className={style.page}>
+      <form className={style.form}>
+        <h3>Новый контакт</h3>
+        <input 
+          type="text" 
+          placeholder="Имя" 
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <input 
+          type="text" 
+          placeholder="Телефон" 
+          value={phone}
+          onChange={(e) => setPhone(e.target.value)}
+        />
+        <button
+          onClick={handleAdd}
+        >
+          Добавить
+        </button>
+      </form>
+      <div className={style.list}>
+      { contacts.map(c => <Card key={c.id} contact={c}/>) }
+      </div>
     </div>
   )
 }
